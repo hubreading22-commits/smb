a2081224f1473044460fc6760188acbe74f2b2d9
hubreading22-commits <hubreading22gmail.com>
2026-06-28 15:08:45 +0530

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -74,8 +74,8 @@ android {
         applicationId = "com.digihub.smbexplorer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 1.0.1
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
